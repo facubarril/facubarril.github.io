31cabfa45cbefbc9797dfdb3d3febbdffc083ac0
Facundo B <facundob@kudosestudio.com>
2022-11-17 00:08:22 -0300

    Updates

---
+++ precache-manifest.cdf843bfec5ed7517c6e960df06f21d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db723c118923c419a293",
+    "url": "/static/css/main.ff582de0.chunk.css"
+  },
+  {
+    "revision": "db723c118923c419a293",
+    "url": "/static/js/main.db723c11.chunk.js"
+  },
+  {
+    "revision": "0177c73d88157ff8bca6",
+    "url": "/static/js/1.0177c73d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "bec5a48da8181c6735d42cd0c4037f8f",
+    "url": "/index.html"
+  }
+];
