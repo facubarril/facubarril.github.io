28b5260cf6465c4d312ac9b1c00737ab53f43eef
Facundo B <facundob@kudosestudio.com>
2022-11-17 00:02:53 -0300

    Updates

---
+++ precache-manifest.9de79a2fbbbd382fe8118f95a03f9d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db723c118923c419a293",
+    "url": "/airsoft-argentina/static/css/main.ff582de0.chunk.css"
+  },
+  {
+    "revision": "db723c118923c419a293",
+    "url": "/airsoft-argentina/static/js/main.db723c11.chunk.js"
+  },
+  {
+    "revision": "0177c73d88157ff8bca6",
+    "url": "/airsoft-argentina/static/js/1.0177c73d.chunk.js"
+  },
+  {
+    "revision": "9b20bb73c4d911609013",
+    "url": "/airsoft-argentina/static/js/runtime~main.9b20bb73.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/airsoft-argentina/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1167af8d1b1a2112ad03fb8d6d1de1ac",
+    "url": "/airsoft-argentina/index.html"
+  }
+];
